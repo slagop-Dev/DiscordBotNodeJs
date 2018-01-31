7981a2f71c5f713e611cb8da14b7993ea4a5189a
MonsterMannen <viktorsvensson94@gmail.com>
2018-01-31 02:08:59 +0100

    need to fix local test again xD

--- commands/play.js
+++ commands/play.js
@@ -3,6 +3,15 @@ var main = require('./../bot.js');
 exports.execute = (client, message, args) => {
     var voiceChannel = message.member.voiceChannel;
 
+    if(voiceChannel == null){
+        message.channel.send("You are not in a voice channel :(");
+        return;
+    }
+    if(args.length < 2){
+        message.channel.send("Specify a song to play :(");
+        return;
+    }
+
     // search string
     var searchString = "";
     for(var i = 1; i < args.length; i++){
@@ -11,23 +20,17 @@ exports.execute = (client, message, args) => {
 
     // already in voice channel
     if(voiceChannel.connection){
-        main.playSong(voiceChannel.connection, message.guild.id, searchString);
+        main.queueSong(voiceChannel.connection, message.guild.id, searchString);
         return;
     }
 
-    if(voiceChannel == null){
-        message.channel.send("You are not in a voice channel :(");
-        return;
-    }
+    // might be true if already joined? leave down here for now
     if(!voiceChannel.joinable){
         message.channel.send("Not allowed to join :(");
         return;
     }
-    if(args.length < 2){
-        message.channel.send("Specify a song to play :(");
-        return;
-    }
 
+    // not in a voice channel, create new connection
     voiceChannel.join()
         .then(connection => {
             console.log("--> Joined voice channel: " + voiceChannel.name);
