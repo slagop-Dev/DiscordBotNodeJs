a5568d6390797246adf371e2dc0ed2de49c514ef
Viktor Svensson <viktorsvensson94@gmail.com>
2018-03-31 03:27:49 +0200

    add april fools event stuff

---
+++ commands/aprilfools.js
@@ -0,0 +1,14 @@
+/*
+    April fools stuff xD
+*/
+
+exports.execute = (client, message, args) => {
+    message.channel.send(":scream::scream::scream: NEW LEVEL SYSTEM ADDED TO BOT :scream::scream::scream: @here\n\nYou need to be level :two: to use this command :sparkles: :flushed: \n:clap: Donate :clap: :five::zero: dollars  :heavy_dollar_sign::heavy_dollar_sign::heavy_dollar_sign: :clap: to :clap: level :clap: up! :100: :100: :100: \n\n  ENJOY :joy::joy::joy:");
+};
+
+exports.info = {
+    name: "aprilfool",
+    alias: ["april", "fool", "aprilfools"],
+    permission: "default",
+    type: "hidden"
+};
