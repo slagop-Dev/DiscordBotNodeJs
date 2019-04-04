4a64da1b7c2c3eeff5a6ca4f26e4832a605f29b9
Viktor Svensson <viktorsvensson94@gmail.com>
2019-04-04 21:42:11 +0200

    move palette

--- commands/emoji.js
+++ commands/emoji.js
@@ -1,6 +1,22 @@
 const Jimp = require('jimp');
 const diff = require('color-diff');
-const emojiRgb = require('./../util/emojiRgbValues.js');
+
+emojiRgb = [
+    {R: 255, G: 255, B: 255, E: ":white_large_square:"},    // white
+    {R: 0, G: 0, B: 0, E: ":black_large_square:"},          // black
+    {R: 255, G: 0, B: 0, E: ":red_circle:"},                // red
+    {R: 0, G: 255, B: 0, E: ":green_heart:"},               // green
+    {R: 0, G: 0, B: 255, E: ":blue_heart:"},                // blue
+    {R: 253, G: 203, B: 88, E: ":yellow_heart:"},           // yellow
+    {R: 154, G: 78, B: 28, E: ":briefcase:"},               // brown
+    {R: 102, G: 117, B: 127, E: ":new_moon:"},              // gray
+    {R: 146, G: 102, B: 204, E: ":star_of_david:"},         // purple
+    {R: 89, G: 142, B: 60, E: ":green_apple:"},             // pepe green
+    {R: 92, G: 172, B: 235, E: ":large_blue_circle:"}       // joy emoji tears blue
+    //{R: 255, G: 255, B: 255, E: ""},
+    //{R: 255, G: 255, B: 255, E: ""},
+    //{R: 255, G: 255, B: 255, E: ""},
+];
 
 exports.execute = (client, message, args) => {
     if(args.length < 2){
@@ -33,11 +49,11 @@ exports.execute = (client, message, args) => {
     }
 
     var palette = [];
-    for(var i = 0; i < emojiRgb.values.length; i++){
+    for(var i = 0; i < emojiRgb.length; i++){
         var obj = {
-            R: emojiRgb.values[i].R,
-            G: emojiRgb.values[i].G,
-            B: emojiRgb.values[i].B,
+            R: emojiRgb[i].R,
+            G: emojiRgb[i].G,
+            B: emojiRgb[i].B,
         }
         palette.push(obj);
     }
@@ -71,13 +87,13 @@ exports.execute = (client, message, args) => {
                     for(var i = 0; i < transColors.length; i++){
                         var e2 = Object.values(transColors[i]);
 
-                        for(var j = 0; j < emojiRgb.values.length; j++){
-                            var e1 = Object.values(emojiRgb.values[j]).slice(0, -1);
+                        for(var j = 0; j < emojiRgb.length; j++){
+                            var e1 = Object.values(emojiRgb[j]).slice(0, -1);
                             var e3 = false;
                             if(e1[0] == e2[0] && e1[1] == e2[1] && e1[2] == e2[2]) e3 = true;
 
                             if(e3){
-                                results.push(emojiRgb.values[j].E);
+                                results.push(emojiRgb[j].E);
                                 break;
                             }
                         }
